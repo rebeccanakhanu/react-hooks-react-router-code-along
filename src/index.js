bd107169485d058420aef711527ca9675123467a
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-05-01 23:40:58 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,22 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // Step 1\. Import react-router functions
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,NavLink } from "react-router-dom";
 
-function Home() {
-  return (
+// add basic styling for NavLinks
+const linkstyles = {
+  display: "inline-block",
+  width: "100",
+  padding: "12px",
+margin:"0 6px 6px",
+background: "blue",
+textDecoration: "none",
+color: "white",
+};
+
+/*define the NavBar component */
+function NavBar(){
+  return(
     <div>
-      <h1>Home!</h1>
-    </div>
+       <NavLink to="/"exact
+      //  set exact so that it only set activeStyle when route is deeply equal to link 
+  // add styling to NavLink
+  style={linkstyles}
+  // add prop for activeStyle
+  activeStyle={{
+    background:"darkblue",
+  }}
+  >
+    Home
+
+  </NavLink>
+
+<NavLink
+to="/about"exact
+style={linkstyles}
+activeStyle={{
+  background:"darkblue",
+}}
+>
+  About
+</NavLink>
+
+<NavLink to="/login" exact
+style={linkstyles}
+activeStyle={{
+  background:"darkblue",
+}}
+>
+  login
+</NavLink>
+      </div>
+    
   );
 }
 
+function Home() {
+  return <h1>Home!</h1>;
+}
+
 function About() {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
+  return <h1>This is my about component!</h1>;
 }
 
 function Login() {
@@ -35,14 +78,6 @@ function Login() {
     </div>
   );
 }
-function NavBar() {
-  return (
-    <div>
-      <h1>Navigation</h1>
-    </div>
-  );
-}
-
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
