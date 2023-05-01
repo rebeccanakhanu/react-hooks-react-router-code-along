f95af5356050a5681ce4f293d3ca4cd26d4eb79c
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-05-01 22:59:15 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1\. Import react-router functions
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function Home() {
   return (
@@ -9,8 +11,48 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+function NavBar() {
+  return (
+    <div>
+      <h1>Navigation</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    <NavBar/>
+
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
